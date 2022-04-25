ef02280fb731d569e4b5fca5a9927ec241e7ce48
STERNPORTNOI <101826917+STERNPORTNOI@users.noreply.github.com>
2022-04-25 20:28:08 -0300

    Initial commit

---
+++ Assets/Scripts/MayoríaDeEdad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayoríaDeEdad : MonoBehaviour
+{
+    public int edadUsuario;
+
+    void Start()
+    {
+
+        if (edadUsuario >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+        }
+
+        else
+        {
+            Debug.Log("No, no es mayor de edad");
+        }
+
+    }
+}
